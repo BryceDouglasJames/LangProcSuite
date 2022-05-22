b8c0d5f4054d8800a05797aa25a42497a69cf5ed
ItBeCharlie <charlied713@gmail.com>
2022-05-21 22:07:38 -0400

    NFA to DFA working

--- main.py
+++ main.py
@@ -13,10 +13,7 @@ class Dfa:
     def __str__(self):
         return 'Start State: ' + self.start_state + '\nAccept States: ' + str(self.accept_states) + '\nTable: ' + str(self.table)
 
-    def init_from_file(in_file, trace=False):
-        with open(in_file, "r") as f:
-            file = f.readlines()
-        f.close()
+    def init_from_list(file, trace=False):
         dfa_table = {}
         lines_index = 0
         # First remove all newlines and blank lines
@@ -82,6 +79,12 @@ class Dfa:
             print('[', cur_state, ']', sep='')
 
         return cur_state in self.accept_states
+
+    def init_from_file(in_file, trace=False):
+        with open(in_file, "r") as f:
+            file = f.readlines()
+        f.close()
+        return Dfa.init_from_list(file, trace)
 
 
 class Nfa:
@@ -164,9 +167,64 @@ class Nfa:
 
         return Nfa(start_state, accept_states, nfa_table, lang_list, states_list)
 
-    def nfa_to_dfa():
-        dfa_table = {}
-        cur_set = []
+    def nfa_to_dfa(self):
+        cur_set = ''
+        table_incrementer = 0
+        conversion_table = []  # DFA_Index Set char1 char2...
+
+        conversion_table.append([1, self.start_state])
+        conversion_index = [self.start_state]
+
+        # print(self.table)
+
+        while table_incrementer < len(conversion_table):
+            for char in self.lang_list:
+                # print(char)
+                cur_set = ''
+                converstion_states = conversion_table[table_incrementer][1].split(
+                    ',')
+                states = []
+                # print('cs', converstion_states)
+                for state in converstion_states:
+                    if state in self.table.keys():
+                        if char in self.table[state].keys():
+                            states.append(self.table[state][char])
+                if len(states) == 0:
+                    states = ['']
+                # print('st', states)
+                for locations in states:
+                    for state in locations:
+                        if state not in cur_set:
+                            cur_set += state + ','
+                cur_set = cur_set[:-1]
+                if cur_set not in conversion_index:
+                    conversion_index.append(cur_set)
+                    conversion_table.append([len(conversion_index), cur_set])
+                conversion_table[table_incrementer].append(
+                    conversion_index.index(cur_set)+1)
+            # print(conversion_table)
+            table_incrementer += 1
+
+        # print(conversion_table)
+
+        # Convert conversion_table to dfa list format
+        dfa_list = []
+        dfa_list.append(self.start_state)
+        accept_states = ''
+        for state in conversion_index:
+            states = state.split(',')
+            for sub_state in states:
+                if sub_state in self.accept_states and state not in accept_states:
+                    accept_states += str(conversion_index.index(state)+1) + ','
+        dfa_list.append(accept_states[:-1])
+        for conv in conversion_table:
+            mod_conv = conv[2:]
+            for i in range(len(mod_conv)):
+                dfa_list.append(str(conv[0]) + ' ' +
+                                self.lang_list[i] + ' ' + str(mod_conv[i]))
+
+        # print(dfa_list)
+        return Dfa.init_from_list(dfa_list)
 
     def check_string(self, input, trace=False):
         cur_state = self.start_state
@@ -182,10 +240,15 @@ class Nfa:
 
 dfa = Dfa.init_from_file(in_file)
 
-print(dfa)
+# print(dfa)
 
-# print(dfa.check_string('aabbabaabbbbbaab', True))
+print(dfa.check_string('aabbaab', True))
 
-nfa = Nfa.init_from_file(in_file2, True)
+nfa = Nfa.init_from_file(in_file2)
 
-print(nfa)
+# print(nfa)
+
+dfa2 = nfa.nfa_to_dfa()
+# print(dfa2)
+
+print(dfa2.check_string('aabbaab', True))
